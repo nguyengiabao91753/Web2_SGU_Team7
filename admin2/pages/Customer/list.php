1a1487e038a7cfbcfc2d54da12b7462e990cb7bc
phucnguyen0704 <phucnguyen.07042004@gmail.com>
2024-03-31 00:03:06 +0700

    HPhuc

--- admin2/pages/Customer/list.php
+++ admin2/pages/Customer/list.php
@@ -37,27 +37,112 @@
 
 
 ?>
+<script>
+// Nút thêm(addButton)
+$(document).ready(function() {
+    var addButton = $("#addbutton");
+    var addForm = $("#formadd");
+
+    addButton.click(function() {
+        addForm.slideDown(); // Sử dụng .show() của jQuery để hiển thị form
+    });
+});
+// Nút đóng(removeButton)
+$(document).ready(function() {
+    var removeButton =$("#remove");
+    var addForm =$("#formadd");
+
+    removeButton.click(function(){
+        addForm.slideToggle();
+    });
+});
+// Nút tạo(createButton)
+
+
+</script>
 
 
 
 
 
 <div class="card">
+    <!--addButton and searchButton-->
+    <div class="addform_n_search">
+        <button id="addbutton" class="btn btn-tool">
+            <i class="fa fa-plus-square"></i> <b>Add</b>
+        </button>
+        <button id="searchbutton" class="btn btn-tool">
+            <i class="fa fa-search"></i>
+        </button>
+        <!--addForm-->
+        <form method="post" action="" id="formadd">
+            <!-- Default box -->
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Customer create</h3>
+
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>   
+                        <button type="button" class="btn btn-tool" id="remove">
+                            <i class="fas fa-times"></i>
+                        </button>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>First Name</label>
+                                <input type="text" class="form-control" placeholder="Enter your first name" name="firstname" value="" >
+                            </div>
+
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Last Name</label>
+                                <input type="text" class="form-control" placeholder="Enter your last name" name="lastname" value="">
+                            </div>
+
+
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input type="text" class="form-control" placeholder="Enter email" name="email" value="">
+                    </div>
+
+
+                    <div class="form-group">
+                        <label>Phone</label>
+                        <input type="text" class="form-control" placeholder="Enter phone" name="phone">
+                    </div>
+                    
+                    <div class="form-group">
+                        <label>Address</label>
+                        <input type="text" class="form-control" placeholder="Enter address" name="address">
+                    </div>
+                </div>
+
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary" id="createButton">Create</button>
+                </div>
+            </div>
+
+            <!-- /.card -->
+        </form>
+
+    </div>        
+
     <div class="card-header">
         <h3 class="card-title">Customer list</h3>
-
-        <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                <i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                <i class="fas fa-times"></i>
-            </button>
-        </div>
     </div>
 
     <div class="card-body">
-        <table id="example1" class="table table-bordered table-striped">
+        <table id="example" class="table table-bordered table-striped">
             <thead>
                 <tr>
                     <th>ID</th>
