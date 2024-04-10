187442b069ca1430bb0e1065141ee59d386c3ce2
phucnguyen0704 <phucnguyen.07042004@gmail.com>
2024-04-10 10:21:55 +0700

    phuc

--- admin2/layouts/sidebar.php
+++ admin2/layouts/sidebar.php
@@ -71,7 +71,7 @@
            <ul class="nav nav-treeview">
              
              <li class="nav-item">
-               <a href="index.php?page=pages/Product/list" class="nav-link">
+               <a href="index.php?page=Product/list" class="nav-link">
                  <i class="far fa-circle nav-icon"></i>
                  <p>List</p>
                </a>
