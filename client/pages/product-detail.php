4c8ecdc792c6cbf09bdb66da758dfc8a55dbd513
phucnguyen0704 <phucnguyen.07042004@gmail.com>
2024-05-02 13:00:23 +0700

    phuc

--- client/pages/product-detail.php
+++ client/pages/product-detail.php
@@ -98,7 +98,7 @@ echo '
                                                         <i class="fs-16 zmdi zmdi-minus"></i>
                                                     </div>
             
-                                                    <input class="mtext-104 cl3 txt-center num-product" type="number" name="num-product" value="1">
+                                                    <input class="mtext-104 cl3 txt-center num-product" type="number" name="num-product" value="1" disabled>
             
                                                     <div class="btn-num-product-up cl8 hov-btn3 trans-04 flex-c-m">
                                                         <i class="fs-16 zmdi zmdi-plus"></i>
